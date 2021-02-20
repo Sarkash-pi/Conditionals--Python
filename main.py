bb7357e18b92f416d0ab3c6171be3f1f0c67df4b
Sarkash Talib <sarkash.talib@gmail.com>
2021-02-20 16:00:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+# x = 15
+# y = 7
+# if x > y:
+#   print('Yes')
+
+#============><============
+
+# x = 15 
+# y = 7 
+# if x < y:
+#   print('No')
+
+
+# if 'Hello' in 'Hello World!':
+#   print('Yes')
+
+#============><============
+
+# x = input('Enter first number: ')
+# y = input('Enter second number: ')
+# if x > y:
+#   print('Yes it is greater')
+# elif x < y:
+#   print('No it is less')
+# else:
+#   print('Neither')
+
+#============><============
+
+''' 
+Nested if statements
+'''
+
+# nu = input('Enter a number: ')
+# num = int(nu)
+
+# if num >= 0:
+#   if num == 0:
+#     print('Zero')
+#   else:
+#     print('Positive number')
+# else: 
+#   print('Negative number')
+
+#============><============
+
+'''
+one-line statemants
+'''
+
+# if 7 > 5: print('Yes it is')
+
+#============><============
+
+'''
+Ternary operator
+'''
+
+# This two stastements is does same 
+
+# print('Yes') if 7 > 5 else print('No')
+
+# if 7 > 5:
+#   print('Yes')
+# else:
+#   print('No')
+
+#============><============
+
+#this is a stastement without an outcome 
+
+# if 100 > 50:
+#   pass
